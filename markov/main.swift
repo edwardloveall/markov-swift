26c37b3bfcc3e00a22bca0067291e6de6e56b9f7
Edward Loveall <edward@edwardloveall.com>
2015-10-21 20:52:02 -0400

    Initial Commit

---
+++ markov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  markov
+//
+//  Created by Edward Loveall on 10/21/15.
+//  Copyright © 2015 Edward Loveall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
